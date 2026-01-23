3f25a02470bc47b838e66b7dbe47f14caadd849b
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-01-23 10:06:58 +0530

    database is connected

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const connectdb =async()=>{
+try{
+ await mongoose.connect('mongodb+srv://noxkiller5:Pancham2005@1stdatabase.lgjtkdw.mongodb.net/')
+console.log("data base is successfully connecte ")
+}
+catch(error){
+console.log(error.message);
+process.exit(1);
+
+}
+}
+
+module.exports=connectdb;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app =express();
 const port =3000;
-
-
+const connectdb = require("./database/db.js");
+connectdb();
 
 
 app.listen(port,()=>{console.log ("app is online now ")});

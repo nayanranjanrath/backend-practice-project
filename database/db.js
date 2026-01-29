1b6fe9dff7a434e756d4025cbf60fe4d197eacff
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-01-29 18:11:06 +0530

    faced a problem of  database dns

--- database/db.js
+++ database/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const connectdb =async()=>{
 try{
+    
  await mongoose.connect('mongodb+srv://noxkiller5:Pancham2005@1stdatabase.lgjtkdw.mongodb.net/project')
 console.log("data base is successfully connected ")
+
 }
 catch(error){
 console.log(error.message);

70bd53ce9de5d631f729aff38ecb52df6d07c492
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-01-31 20:46:02 +0530

    just created a method

--- controllers/registeruser.js
+++ controllers/registeruser.js
@@ -2,6 +2,22 @@
 const uploadoncloudinary =require("../middlewares/cloudeinary.middleware.js")
 
 const usermodel = require("../models/usermodel.js")
+const generateaccessandrefreshtokens=async(usreid)=>{
+try{
+const user=usermodel.findById(usreid)
+const accesstoken = user.generateaccesstoken()
+const refreshtoken = user.generaterefreshtoken()
+user.refreshtoken=refreshtoken
+await user.save({validatebeforesave:false})
+
+return{accesstoken,refreshtoken}
+}
+catch(error){console.error(error)
+res.status(400).json({message:"something went wrong "})
+
+}
+
+}
 const registeruser = async (req,res) =>{
 try{
 
@@ -42,5 +58,29 @@ catch(error){  console.error(error);
   
 }
 }
+const loginuser=async(req,res)=>{
+const{username,email,password}=req.body
+if(!username||!email){
+  throw console.error("username or email is required")
+ return res.status(400).json({
+    success:false
+  })
+}
+const user = await usermodel.findOne(
+ { $or: [{username},{email}]}
+)
+if(!user){
+throw console.error("user is new ");
+return res.status(400).json({reson:"user need to login first "})
 
+}
+const ispasswordvalid =await  user.ispasswordcorrect
+if(!ispasswordvalid){
+  throw console.error("password is incorect");
+return res.status(400).json({reson:"password is incorrect "})
+
+}
+const {accesstoken,refreshtoken}=await generateaccessandrefreshtokens(user._id)
+
+}
 module.exports=registeruser

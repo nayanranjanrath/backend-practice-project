7c59f16ba7251645c3496b1418417a4f5459c79c
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-02-16 22:12:19 +0530

    installed socket and changed the index according to socket

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
+const http=require('http')
+const {Server}=require('socket.io')
+
 const cookieParser = require('cookie-parser')
 const express = require('express');
 const app =express();
+const server =http.createServer(app)
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",//this line helps us to connect our server if also our servers are not in the same port 
+  },
+});
+
+
 const port =3000;
 const connectdb = require("./database/db.js");
 connectdb();
@@ -12,4 +25,4 @@ app.use(express.urlencoded({ extended: true })); // for form data
 const registerrouter=require("./routes/registerrouter.js")
 app.use('/api',registerrouter)
 
-app.listen(port,()=>{console.log ("app is online now ")});
+server.listen(port,()=>{console.log ("app is online now ")});
